dee85ef5a3725f3bde722478424de30dfa22c528
Hunny-Saini <124673671+Hunny-Saini@users.noreply.github.com>
2023-03-16 15:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+    var lowerStr = str.toLowerCase();
+	var start = 0;
+	var end = str.length;
+	while (start != end) {
+		if(str.charAt(start) != str.charAt(end)){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
